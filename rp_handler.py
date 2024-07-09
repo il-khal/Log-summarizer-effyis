0549838dd713c2c7118f933e875ddcfd46f3b840
Ilyas <khalloukielias@gmail.com>
2024-07-09 09:19:03 +0000

    Using llama on new data

--- rp_handler.py
+++ rp_handler.py
@@ -19,26 +19,19 @@ alpaca_prompt = """Below is an instruction that describes a task, paired with an
 max_seq_length = 2048
 
 model, tokenizer = FastLanguageModel.from_pretrained(
-    model_name="ozzyable/log-summerizer-gemma-2b-it-4bit",
+    model_name="ozzyable/tinyllama-alt-data",
     max_seq_length=max_seq_length,
     dtype=None,
-    load_in_4bit=True
+    load_in_4bit=False
     )
 
 FastLanguageModel.for_inference(model)
 
 def summarize(transactions: list):
-    print(
-        alpaca_prompt.format(
-                "you are a transaction interpreter, you receive transactions that are written for the banking context & you extract valuable data from them in JSON format: {'transaction_channel': (Transfer, Online Payment, Card Payment, Bank fee, Deposit), 'other_party_name': name of the sender or receiver, 'info': motif or reason of the transaction, if there is no motif just leave it blank}",
-                transaction,
-                ""
-            ) for transaction in transactions
-    )
     inputs = tokenizer(
         [
             alpaca_prompt.format(
-                "you are a transaction interpreter, you receive transactions that are written for the banking context & you extract valuable data from them in JSON format: {'transaction_channel': (Transfer, Online Payment, Card Payment, Bank fee, Deposit), 'other_party_name': name of the sender or receiver, 'info': motif or reason of the transaction, if there is no motif just leave it blank}",
+                "you are a transaction interpreter , you receive transactions that are writen for the banking context & you extract the transaction category: other, financial, pension, reimbursement and salary", # instruction
                 transaction,
                 ""
             ) for transaction in transactions
@@ -47,9 +40,18 @@ def summarize(transactions: list):
         return_tensors="pt"
     ).to("cuda")
 
-    outputs = model.generate(**inputs, max_new_tokens=2048, use_cache=True, num_beams=5)
+    outputs = model.generate(**inputs, max_new_tokens=2048, use_cache=True)
     results = tokenizer.batch_decode(outputs, skip_special_tokens=True)
-    return results
+    s = []
+    
+    for transaction in results:
+        substring = "Response:"
+        start_index = output.find(substring) + len(substring)
+
+        formatted_output = output[start_index:]
+        s.append(formatted_output)
+
+    return s
 
 
 def process_input(input):
