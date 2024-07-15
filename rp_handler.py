d282f96dd4ad586f2a5e61bd4c732f9a2204a645
Ilyas <khalloukielias@gmail.com>
2024-07-15 08:24:57 +0000

    Commiting the code for the stable version of gemma

--- rp_handler.py
+++ rp_handler.py
@@ -1,69 +1,57 @@
 import torch._dynamo
-torch._dynamo.config.suppress_errors = True # suppressing errors : safe  mesure for unexpected torch errors
+torch._dynamo.config.suppress_errors = True
 
 
 import runpod
 from unsloth import FastLanguageModel
+import asyncio
 
 alpaca_prompt = """Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
-
 ### Instruction:
 {}
-
 ### Input:
 {}
-
-### Response:
+### Response:<bor>
 """
 
 max_seq_length = 2048
-
 model, tokenizer = FastLanguageModel.from_pretrained(
-    # othermodels are at https://huggingface.co/ozzyable
     model_name="ozzyable/log-summerizer-gemma-2b-it-4bit",
     max_seq_length=max_seq_length,
     dtype=None,
     load_in_4bit=True
-    )
+)
+
+model.save_pretrained("ozzyable/log-summerizer-gemma-2b-it-4bit")
+tokenizer.save_pretrained("ozzyable/log-summerizer-gemma-2b-it-4bit")
 
 FastLanguageModel.for_inference(model)
 
-def summarize(transaction: str):
+async def summarize(transaction: str):
     inputs = tokenizer(
         [
             alpaca_prompt.format(
-                "you are a transaction interpreter , you receive transactions that are writen for the banking context & you extract valuable data them in json format: {'transaction_channel': (Transfer, Online Payement, Card Payement, Bank fee, Deposit), 'other_party_name': name of the sender or receiver, 'info': motif or reason of the transaction, if there is no motif just leave it blank}", # instruction
+                "you are a transaction interpreter, you receive transactions that are written for the banking context & you extract valuable data from them in JSON format: {'transaction_channel': (Transfer, Online Payment, Card Payment, Bank fee, Deposit), 'other_party_name': name of the sender or receiver, 'info': motif or reason of the transaction, if there is no motif just leave it blank}",
                 transaction,
                 ""
             )
-        ],
-        padding=True,
-        return_tensors="pt"
-    ).to("cuda")
+        ], return_tensors="pt").to("cuda")
 
-    outputs = model.generate(**inputs, max_new_tokens=2048, use_cache=True)
-    result = tokenizer.batch_decode(outputs, skip_special_tokens=True)[0]
-    
-    substring = "{'transaction_channel': '"
-    start_index = result.find(substring)
+    outputs = model.generate(**inputs, max_new_tokens=1024, use_cache=True)
+    result = tokenizer.batch_decode(outputs)[0]
 
-    formatted_output = result[start_index:]
-
-    return formatted_output
+    result = result[result.find("<bor>") + 5:result.rfind("<eos>")].strip()
+    return result
 
 
-def process_input(input):
+async def process_input(input):
     transactions = input.get('transaction', [])
-    results = []
-    for transaction in transactions:
-        results.append(summarize(transaction))
+    results = await asyncio.gather(*[summarize(t) for t in transactions])
 
     return results
 
-def handler(event):
-    # "event" is the input the pod receives
-    # returns a list of strings, each string contains the unparsed json for each transaction
-    return process_input(event['input'])
+async def handler(event):
+    return await process_input(event['input'])
 
 if __name__ == '__main__':
     runpod.serverless.start({'handler': handler})
