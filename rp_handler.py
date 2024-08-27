d8cd3f9b8bb11a569360bd783835a4f401080c2c
Ilyas <khalloukielias@gmail.com>
2024-08-27 15:58:09 +0000

    Not caring anymore about the transaction content

--- rp_handler.py
+++ rp_handler.py
@@ -1,7 +1,6 @@
 import torch._dynamo
 torch._dynamo.config.suppress_errors = True
 
-
 import runpod
 from unsloth import FastLanguageModel
 import asyncio
@@ -24,28 +23,27 @@ model, tokenizer = FastLanguageModel.from_pretrained(
 
 FastLanguageModel.for_inference(model)
 
-async def summarize(transaction: dict):
+async def summarize(transaction):
+    description = transaction.get("descriptionTransaction", "")
+    
     inputs = tokenizer(
-        [
-            alpaca_prompt.format(
-                "you are a transaction interpreter, you receive transactions that are written for the banking context & you extract valuable data from them in JSON format: {'transaction_channel': (Transfer, Online Payment, Card Payment, Bank fee, Deposit), 'other_party_name': name of the sender or receiver, 'info': motif or reason of the transaction, if there is no motif just leave it blank}",
-                transaction.get("description"),
-                ""
-            )
-        ], return_tensors="pt").to("cuda")
+        alpaca_prompt.format(
+            "you are a transaction interpreter, you receive transactions that are written for the banking context & you extract valuable data from them in JSON format: {'transaction_channel': (Transfer, Online Payment, Card Payment, Bank fee, Deposit), 'other_party_name': name of the sender or receiver, 'info': motif or reason of the transaction, if there is no motif just leave it blank}",
+            description,
+            ""
+        ),
+        return_tensors="pt"
+    ).to("cuda")
 
     outputs = model.generate(**inputs, max_new_tokens=1024, use_cache=True)
-    result = tokenizer.batch_decode(outputs)[0]
+    result = tokenizer.decode(outputs[0], skip_special_tokens=True)
 
-    result = result[result.find("<bor>") + 5:result.rfind("<eos>")].strip()
-    return {'id': transaction.get('id'), 'summary': result}
-
+    transaction['summary'] = result[result.find("<bor>") + 5:].strip()
+    return transaction
 
 async def process_input(input):
     transactions = input.get('transaction', [])
-    results = await asyncio.gather(*[summarize(t) for t in transactions])
-
-    return results
+    return await asyncio.gather(*map(summarize, transactions))
 
 async def handler(event):
     return await process_input(event['input'])
