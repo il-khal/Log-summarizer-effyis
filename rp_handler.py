54fb31f0e1352c257ba66b9875cb7066f1ba7c23
Ilyas <khalloukielias@gmail.com>
2024-08-27 16:55:59 +0000

    Setting up the new model

--- rp_handler.py
+++ rp_handler.py
@@ -15,7 +15,7 @@ alpaca_prompt = """Below is an instruction that describes a task, paired with an
 
 max_seq_length = 2048
 model, tokenizer = FastLanguageModel.from_pretrained(
-    model_name="ozzyable/log-summerizer-gemma-2b-it-4bit",
+    model_name="ozzyable/log-summerizer-tinyllama-v3",
     max_seq_length=max_seq_length,
     dtype=None,
     load_in_4bit=True
@@ -23,27 +23,32 @@ model, tokenizer = FastLanguageModel.from_pretrained(
 
 FastLanguageModel.for_inference(model)
 
-async def summarize(transaction):
-    description = transaction.get("descriptionTransaction", "")
+def summarize(transactions):
+    formatted_inputs = [
+        alpaca_prompt.format(
+            "you are a transaction interpreter , you receive transactions that are writen for the banking context & you extract valuable data them in json format: {'transaction_channel': (Transfer, Online Payement, Card Payement, Bank fee, Deposit), 'other_party_name': name of the sender or receiver, 'info': motif or reason of the transaction, if there is no motif just leave it blank}", # instruction
+            f"{transaction}", # input
+            ""
+        )
+        for transaction in transactions
+    ]
     
     inputs = tokenizer(
-        alpaca_prompt.format(
-            "you are a transaction interpreter, you receive transactions that are written for the banking context & you extract valuable data from them in JSON format: {'transaction_channel': (Transfer, Online Payment, Card Payment, Bank fee, Deposit), 'other_party_name': name of the sender or receiver, 'info': motif or reason of the transaction, if there is no motif just leave it blank}",
-            description,
-            ""
-        ),
+        formatted_inputs,
         return_tensors="pt"
     ).to("cuda")
 
     outputs = model.generate(**inputs, max_new_tokens=1024, use_cache=True)
-    result = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    result = tokenizer.batch_decode(outputs[0], skip_special_tokens=True)
 
-    transaction['summary'] = result[result.find("<bor>") + 5:].strip()
-    return transaction
+    for i in range(len(transactions)):
+        transactions[i]['summary'] = result[i][result.find("<bor>") + 5:].strip()
+
+    return transactions
 
 async def process_input(input):
     transactions = input.get('transaction', [])
-    return await asyncio.gather(*map(summarize, transactions))
+    return summarize(transactions)
 
 async def handler(event):
     return await process_input(event['input'])
